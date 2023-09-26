5e297eb7e69e645bdb4d63dff8275aae6675ab6f
Leofigueiredoo <56490782+Leofigueiredoo@users.noreply.github.com>
2023-09-26 17:08:49 -0300

    Update BancoUsuarios.sql

--- BancoUsuarios.sql
+++ BancoUsuarios.sql
@@ -3,3 +3,13 @@ CREATE TABLE Usuarios (
     nome VARCHAR(255) NOT NULL,
     telefone VARCHAR(15) NOT NULL
 );
+-- Inserir um usuário
+INSERT INTO Usuarios (nome, telefone) VALUES ('João', '(11) 1234-5678');
+
+-- Inserir outro usuário
+INSERT INTO Usuarios (nome, telefone) VALUES ('Maria', '(21) 9876-5432');
+
+-- Inserir mais usuários
+INSERT INTO Usuarios (nome, telefone) VALUES ('Carlos', '(31) 5555-9999');
+INSERT INTO Usuarios (nome, telefone) VALUES ('Ana', '(41) 7777-1234');
+
